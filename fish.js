803614872a4eacf4ea1b7bfa921b8e567c48637f
Rafal Kachniarz <rafal.kachniarz@unic.com>
2023-07-30 02:11:51 +0200

    Initial commit - playing around

---
+++ fish.js
@@ -0,0 +1,19 @@
+// import { getRandomNumber } from "./helpers.js"
+
+// const sizes = ['Tiny', 'Small', 'Regular Size', 'Large', 'Gigantic', 'Humongous']
+
+// class Fish {
+//     constructor(){
+//         this.name = name;
+//         this.size = sizes[getRandomNumber(1,sizes.length)];
+//     }
+    
+//     listAttributes() {
+//         return `Name: ${this.name}, Rarity: ${this.size}`
+//     }
+
+
+
+// }
+
+// export const fish = new Fish();
---
+++ fishNames.js
@@ -0,0 +1,19 @@
+export default {
+    "names": [
+        "Carp",
+        "Albacore",
+        "Roach",
+        "Sea Bass",
+        "Freshwater Bass",
+        "Pike",
+        "Catfish",
+        "Salmon",
+        "Trout",
+        "Sturgeon",
+        "Mackarel",
+        "Herring"
+    ]
+
+}
+
+
