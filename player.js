9674b1f25e9e562a6ad9fd2f7f9f1e9014bfee10
Rafal Kachniarz <rafal.kachniarz@unic.com>
2023-08-20 20:44:15 +0200

    1

--- player.js
+++ player.js
@@ -2,10 +2,41 @@ import { getRandomNumber, roll20 } from "./helpers.js";
 
 const xpToLevel = [0, 10, 25, 50, 100, 250, 500, 1000, 2000, 5000];
 
+const classes = [{
+    name: 'Fish Fighter',
+    skillmin: 2,
+    skillmax: 7,
+    treasureFindMin: 0,
+    treasureFindMax: 5,
+    fishFindMin: 1,
+    fishFindMax: 5
+}, 
+{
+    name: 'Fishing Wizard',
+    skillmin: 1,
+    skillmax: 5,
+    treasureFindMin: 1,
+    treasureFindMax: 5,
+    fishFindMin: 5,
+    fishFindMax: 10
+},
+{
+    name: 'Rogue Fisherman',
+    skillmin: 0,
+    skillmax: 5,
+    treasureFindMin: 5,
+    treasureFindMax: 10,
+    fishFindMin: 0,
+    fishFindMax: 5
+}]
+
 class Player {
-    constructor(name="Player"){
+    constructor(name="Player", classId=0){
         this.name = name; 
-        this.skill = getRandomNumber(0, 5);
+        this.class = classes[classId];
+        this.skill = getRandomNumber(this.class.skillmin, this.class.skillmax);
+        this.treasureFind = getRandomNumber(this.class.treasureFindMin, this.class.treasureFindMax);
+        this.fishFind = getRandomNumber(this.class.fishFindMin, this.class.fishFindMax);
         this.totalxp = 0;
         this.level = 0;
         this.fishHistory = [];
@@ -16,7 +47,7 @@ class Player {
     gainXP(gain) {
         this.totalxp += gain;
         if (this.level === xpToLevel.length) return false; 
-        if (this.totalxp > xpToLevel[this.level+1]) {
+        if (this.totalxp >= xpToLevel[this.level+1]) {
             this.level +=1;
             return true;
         } else return false;
